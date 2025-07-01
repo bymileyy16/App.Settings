5f5f0558c3fa1035a56a2365cda404220e9f2e5f
Emily Menezes <menezesemily2008@gmail.com>
2025-07-01 16:32:47 -0300

    atividade

---
+++ App.settings/Models/Tarefa.cs
@@ -0,0 +1,8 @@
+namespace App.settings.Models;
+
+public class Tarefa
+{
+    public int id { get; set; }
+    public string nome { get; set; } = string.Empty;
+    public bool concluida { get; set; }
+}
